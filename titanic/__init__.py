e50c44ebfe0325cf6142f6494fd076b23a5afc66
hjuju <tkackeowjs@naver.com>
2021-05-31 17:56:30 +0900

    new

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -1,17 +1,24 @@
 from titanic.views.controller import Controller
+from titanic.templates.plot import Plot
 
 if __name__ == '__main__':
-    controller = Controller()
     while 1:
         menu = input('0. Exit\n'
-                     '1. Pre_process\n'
+                     '1. data visualization\n'
+                     '2. modeling\n'
+                     '3. machine learning\n'
+                     '4. machine release\n'
                      '번호입력: ')
         if menu == '0':
             break
         elif menu == '1':
-            controller.preprocessing('train.csv')
+            plot = Plot('train.csv')
+            plot.draw_survived_dead()
+            plot.draw_pclass()
+            plot.draw_sex()
+            plot.draw_embarked()
         elif menu == '2':
-            pass
+            controller = Controller().modeling('train.csv', 'test.csv')
         elif menu == '3':
             pass
         elif menu == '4':
