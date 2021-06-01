7d6673428c5bdc17b623ab3cafe4f0d5992ef4b0
hjuju <tkackeowjs@naver.com>
2021-06-01 18:11:52 +0900

    new

--- titanic/views/controller.py
+++ titanic/views/controller.py
@@ -17,10 +17,30 @@ class Controller(object):
     def preprocess(self, train, test) -> object:
         service = self.service
         this = self.dataset
+        # 초기모델 생성
         this.train = service.new_model(train)
         this.test = service.new_model(test)
-        print(f'Train 의 Type 은 {type(this.train)} 이다.')
-        print(f'Train 의 column 은 {this.train.columns} 이다.')
-        print(f'Test 의 Type 은 {type(this.test)} 이다.')
-        print(f'Test 의 column 은 {this.test.columns} 이다.')
+        # 불필요한 feature (Cabin, Ticket)제거
+        this = service.drop_feature(this, 'Ticket')
+        this = service.drop_feature(this, 'Cabin')
+        # normianl, ordinal로 정형화
+        this = service.embarked_nominal(this)
+        this = service.title_norminal(this)
+        # 불필요한 feature (Name)제거
+        this = service.drop_feature(this, 'Name')
+        this = service.gender_norminal(this)
+        this = service.drop_feature(this, 'Sex')
+        self.print_this(this)
+        # 위와같은 순서(프로세스)가 존재 -> 알고리즘
         return this
+
+    @staticmethod
+    def print_this(this):
+        print('*'*100)
+        print(f'Train 의 Type 은 {type(this.train)} ')
+        print(f'Train 의 column 은 {this.train.columns} ')
+        print(f'Train 의 상위 5개 행은 \n{this.train.head()} ')
+        print(f'Test 의 Type 은 {type(this.test)} ')
+        print(f'Test 의 column 은 {this.test.columns} ')
+        print(f'Test 의 상위 5개 행은 \n{this.test.head()} ')
+        print('*'*100)
